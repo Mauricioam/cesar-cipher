f0dd7354376990e8ecb4f084200d4c12d246afc6
Mauricioam <mauricioam91@gmail.com>
2026-06-09 00:06:00 +0000

    notes about how to resolve the exercise

---
+++ index.rb
@@ -0,0 +1,8 @@
+# segun entiendo, es una funcion que, toma un string como arg y un numero
+# el numero indica las posiciones que se debe mover, tomando la primera letra del arg
+# e inntercambiar por ese posición en el abecedario
+# ejemplo cesa_cipher("a",3) --> d
+# INICIO algoritmo cesar_cipher(strg,n)
+# alphabet <-- [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,x,y,z]
+# split_word <-- strg.split
+# 
