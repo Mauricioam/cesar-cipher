3a65967e61847e50c8ca7f4e03d7ab4e091d91d9
Mauricioam <mauricioam91@gmail.com>
2026-06-09 23:58:36 +0000

    Added loop for replacing letters for correspond key that equal the position in the alphabet. Example B key = 1 ==> C. Also added edge cases where the pos + key > lenght of alphabet.

--- index.rb
+++ index.rb
@@ -1,25 +1,20 @@
-require 'pry-byebug'
-# segun entiendo, es una funcion que, toma un string como arg y un numero
-# el numero indica las posiciones que se debe mover, tomando la primera letra del arg
-# e inntercambiar por ese posición en el abecedario
-# ejemplo cesa_cipher("a",3) --> d
-# INICIO algoritmo cesar_cipher(strg,n)
-# alphabet <-- [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,x,y,z]
-# split_word <-- strg.split
+
 
 def cesar_cipher(letter, key)
- eng_alphabet_upp = ("A".."Z").to_a
+  eng_alphabet_upp = ("A".."Z").to_a
 
-
-eng_alphabet_upp.each_with_index { |word, idx| 
-if ( word === letter)
-    if(key + idx > eng_alphabet_upp.length)
-        puts "we need to wrap the array"
-    else
-        puts "Its below 27"
+	## loop for replacing string with corresponding key
+  eng_alphabet_upp.each_with_index do |word, idx| 
+    if word == letter
+      if key + idx >= eng_alphabet_upp.length
+        letter = eng_alphabet_upp[idx + key - eng_alphabet_upp.length]
+        puts letter
+      else
+        letter = eng_alphabet_upp[idx + key]
+      end
     end
-end
-}
+  end
 end
 
-cesar_cipher("U",7)
+
+cesar_cipher("U",6)
